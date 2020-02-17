cd0a2150686646f22d80cd70879cb467f64e5125
Hrant-p <hrant@live.ru>
2020-02-17 23:09:31 +0400

    webpack configurations for React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div>
+            <p>
+                This is React App
+            </p>
+        </div>
+    );
+};
+
+export default App;
